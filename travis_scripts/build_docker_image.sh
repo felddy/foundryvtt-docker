38e4868e1aa3b1e52765c5337879baba702b5f4f
Felddy <markf+github@geekpad.com>
2019-06-03 21:48:03 -0400

    Add version slices to docker deploy

--- travis_scripts/build_docker_image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-
-version=$(./bump_version.sh show)
-# shellcheck disable=SC2140
-docker build -t "$IMAGE_NAME":"$version" -t "$IMAGE_NAME":"latest" .
