293b370a2b5bca53461b85dc7e73e5f94efb3d51
Felddy <markf@geekpad.com>
2020-05-24 10:25:08 -0400

    Add setup.py and integrate with requirement file chain.

---
+++ setup.py
@@ -0,0 +1,88 @@
+"""
+This is the setup module for the foundry-vtt docker project.
+
+Based on:
+
+- https://packaging.python.org/distributing/
+- https://github.com/pypa/sampleproject/blob/master/setup.py
+- https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
+"""
+
+# Standard Python Libraries
+from glob import glob
+from os.path import basename, splitext
+
+# Third-Party Libraries
+from setuptools import find_packages, setup
+
+
+def readme():
+    """Read in and return the contents of the project's README.md file."""
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
+
+def package_vars(version_file):
+    """Read in and return the variables defined by the version_file."""
+    pkg_vars = {}
+    with open(version_file) as f:
+        exec(f.read(), pkg_vars)  # nosec
+    return pkg_vars
+
+
+setup(
+    name="foundryvtt_docker",
+    # Versions should comply with PEP440
+    version=package_vars("src/_version.py")["__version__"],
+    description="foundryvtt_docker python library",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/felddy",
+    # The project's main homepage
+    download_url="https://github.com/felddy/foundryvtt-docker",
+    # Author details
+    author="Mark Feldhousen",
+    author_email="markf@geekpad.com",
+    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: System Administrators",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Games/Entertainment :: Role-Playing",
+    ],
+    python_requires=">=3.6",
+    # What does your project relate to?
+    keywords="foundryvtt",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    install_requires=["docker-compose", "docopt", "setuptools >= 24.2.0", "schema"],
+    extras_require={
+        "test": [
+            "pre-commit",
+            # coveralls 1.11.0 added a service number for calls from
+            # GitHub Actions. This caused a regression which resulted in a 422
+            # response from the coveralls API with the message:
+            # Unprocessable Entity for url: https://coveralls.io/api/v1/jobs
+            # 1.11.1 fixed this issue, but to ensure expected behavior we'll pin
+            # to never grab the regression version.
+            "coveralls != 1.11.0",
+            "coverage",
+            "pytest-dockerc",
+            "pytest-cov",
+            "pytest",
+        ]
+    },
+    # Conveniently allows one to run the CLI tool as `foundryvtt_docker`
+    entry_points={
+        # "console_scripts": ["download_release = download_release:main"]
+    },
+)
