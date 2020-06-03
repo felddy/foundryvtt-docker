d44eb3a13c2d1fd15902b9cbee17cd983004430e
Felddy <markf@geekpad.com>
2020-06-02 21:22:00 -0400

    Remove unneeded linter directive.  Add smarm.

--- src/set_password.js
+++ src/set_password.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 
 const crypto = require("crypto");
 const fs = require("fs");
@@ -7,12 +6,12 @@ const fs = require("fs");
 const digest = "sha512";
 const iterations = 1000;
 const keylen = 64;
-const salt = "17c4f39053ac5a50d5797c665ad1f4e6";
+const low_sodium = "17c4f39053ac5a50d5797c665ad1f4e6";
 
 var plaintext = fs.readFileSync(process.stdin.fd, "utf-8");
 var cyphertext = crypto.pbkdf2Sync(
   plaintext.trim(),
-  salt,
+  low_sodium,
   iterations,
   keylen,
   digest
