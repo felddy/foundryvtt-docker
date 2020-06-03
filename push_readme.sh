4950c746947ae855ef847ac444f4fdf352e5d4e6
Felddy <markf@geekpad.com>
2020-06-03 11:32:49 -0400

    Replace ancient readme publisher.

--- push_readme.sh
+++ push_readme.sh
@@ -9,15 +9,17 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+echo "Logging in and requesting JWT..."
 token=$(curl -s -X POST \
   -H "Content-Type: application/json" \
   -d '{"username": "'"$DOCKER_USERNAME"'", "password": "'"$DOCKER_PASSWORD"'"}' \
   https://hub.docker.com/v2/users/login/ | jq -r .token)
 
+echo "Pushing README file..."
 code=$(jq -n --arg msg "$(<README.md)" \
   '{"registry":"registry-1.docker.io","full_description": $msg }' | \
-      curl -s -o /dev/null  -L -w "%{http_code}" \
-         https://cloud.docker.com/v2/repositories/"${IMAGE_NAME}"/ \
+      curl -s -o /dev/null -L -w "%{http_code}" \
+         https://hub.docker.com/v2/repositories/"${IMAGE_NAME}"/ \
          -d @- -X PATCH \
          -H "Content-Type: application/json" \
          -H "Authorization: JWT ${token}")
