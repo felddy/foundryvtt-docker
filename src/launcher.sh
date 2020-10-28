70ead2b6f15a0dfe0bdb9037f90f03e189d8154b
Mark Feldhousen <felddy@users.noreply.github.com>
2020-10-28 11:34:46 -0400

    Modify semanitcs of CONTAINER_PRESERVE_CONFIG environment variable (#100)
    * Modify behavior of CONTAINER_PRESERVE_CONFIG.
    
    This addresses issue #99
    
    We will now check to see if each of the files exist individually.  If 
    they do not yet exist, they will be created as normal.
    
    * Clarify log messages, and rework constants.
    
    * Move shell drop after configuration management.
    
    * Add log message to --root-shell processing.

--- src/launcher.sh
+++ src/launcher.sh
@@ -6,7 +6,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# setup logging
+CONFIG_DIR="/data/Config"
+ADMIN_KEY_FILE="${CONFIG_DIR}/admin.txt"
+CONFIG_FILE="${CONFIG_DIR}/options.json"
 # shellcheck disable=SC2034
 # LOG_NAME used in sourced file
 LOG_NAME="Launcher"
@@ -14,30 +16,36 @@ LOG_NAME="Launcher"
 # disable following
 source logging.sh
 
-if [ "$1" = "--shell" ]; then
-  /bin/sh
-  exit $?
-fi
+# ensure the config directory exists
+mkdir -p "${CONFIG_DIR}" >& /dev/null
 
-if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" ]]; then
-  log_warn "CONTAINER_PRESERVE_CONFIG set to true."
-  log_warn "options.json and admin.txt will not be modified."
+if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" && -f "${CONFIG_FILE}" ]]; then
+  log_warn "CONTAINER_PRESERVE_CONFIG is set: Not updating options.json"
 else
   # Update configuration file
-  mkdir -p /data/Config >& /dev/null
   log "Generating options.json file."
-  ./set_options.js > /data/Config/options.json
+  ./set_options.js > "${CONFIG_FILE}"
+fi
 
+if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" && -f "${ADMIN_KEY_FILE}" ]];
+then
+  log_warn "CONTAINER_PRESERVE_CONFIG is set: Not updating admin.txt"
+else
   # Save admin access key to file if set.  Delete file if unset.
   if [[ "${FOUNDRY_ADMIN_KEY:-}" ]]; then
     log "Setting 'Admin Access Key'."
-    echo "${FOUNDRY_ADMIN_KEY}" | ./set_password.js > /data/Config/admin.txt
+    echo "${FOUNDRY_ADMIN_KEY}" | ./set_password.js > "${ADMIN_KEY_FILE}"
   else
     log_warn "No 'Admin Access Key' has been configured."
-    rm /data/Config/admin.txt >& /dev/null || true
+    rm "${ADMIN_KEY_FILE}" >& /dev/null || true
   fi
+fi
 
-fi #CONTAINER_PRESERVE_CONFIG
+if [ "$1" = "--shell" ]; then
+  log_warn "Starting a shell as requested by argument --shell"
+  /bin/sh
+  exit $?
+fi
 
 # Spawn node with clean environment to prevent credential leaks
 log "Starting Foundry Virtual Tabletop."
