197bd34df381d0c541084f72e897c2ef20d0bbd0
Felddy <markf@geekpad.com>
2020-10-27 22:24:26 -0400

    Modify behavior of CONTAINER_PRESERVE_CONFIG.
    This addresses issue #99
    
    We will now check to see if each of the files exist individually.  If 
    they do not yet exist, they will be created as normal.

--- src/launcher.sh
+++ src/launcher.sh
@@ -6,7 +6,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# setup logging
+ADMIN_KEY_FILE="/data/Config/admin.txt"
+CONFIG_FILE="/data/Config/options.json"
 # shellcheck disable=SC2034
 # LOG_NAME used in sourced file
 LOG_NAME="Launcher"
@@ -19,25 +20,30 @@ if [ "$1" = "--shell" ]; then
   exit $?
 fi
 
-if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" ]]; then
-  log_warn "CONTAINER_PRESERVE_CONFIG set to true."
-  log_warn "options.json and admin.txt will not be modified."
+# ensure the config directory exists
+mkdir -p /data/Config >& /dev/null
+
+if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" && -f "${CONFIG_FILE}" ]]; then
+  log_warn "CONTAINER_PRESERVE_CONFIG is set: preserving existing options.json"
 else
   # Update configuration file
-  mkdir -p /data/Config >& /dev/null
   log "Generating options.json file."
-  ./set_options.js > /data/Config/options.json
+  ./set_options.js > "${CONFIG_FILE}"
+fi
 
+if [[ "${CONTAINER_PRESERVE_CONFIG:-}" == "true" && -f "${ADMIN_KEY_FILE}" ]];
+then
+  log_warn "CONTAINER_PRESERVE_CONFIG is set: preserving existing admin.txt"
+else
   # Save admin access key to file if set.  Delete file if unset.
   if [[ "${FOUNDRY_ADMIN_KEY:-}" ]]; then
     log "Setting 'Admin Access Key'."
-    echo "${FOUNDRY_ADMIN_KEY}" | ./set_password.js > /data/Config/admin.txt
+    echo "${FOUNDRY_ADMIN_KEY}" | ./set_password.js > "${ADMIN_KEY_FILE}"
   else
     log_warn "No 'Admin Access Key' has been configured."
-    rm /data/Config/admin.txt >& /dev/null || true
+    rm "${ADMIN_KEY_FILE}" >& /dev/null || true
   fi
-
-fi #CONTAINER_PRESERVE_CONFIG
+fi
 
 # Spawn node with clean environment to prevent credential leaks
 log "Starting Foundry Virtual Tabletop."
