fc5897e83d6c52072c4c48dff4169afb3ea54573
Felddy <markf@geekpad.com>
2020-12-08 18:22:49 -0500

    Add log_debug function.
    Add conditional debug logging when CONTAINER_VERBOSE is set.

--- src/logging.sh
+++ src/logging.sh
@@ -3,12 +3,19 @@
 # busybox supports more features than POSIX /bin/sh
 
 # Define terminal colors for use in logger functions
+BLUE="\e[34m"
 GREEN="\e[32m"
 RED="\e[31m"
 RESET="\e[0m"
 YELLOW="\e[33m"
 
 # Mimic the winston logging used in logging.js
+log_debug(){
+  if [[ "${CONTAINER_VERBOSE:-}" ]]; then
+    echo -e "${LOG_NAME} | $(date +%Y-%m-%d\ %H:%M:%S) | [${BLUE}debug${RESET}] $*"
+  fi
+}
+
 log(){
   echo -e "${LOG_NAME} | $(date +%Y-%m-%d\ %H:%M:%S) | [${GREEN}info${RESET}] $*"
 }
