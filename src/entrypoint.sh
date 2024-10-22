ab461df436c85645357697b630ab049499c9c80d
Felddy <markf@geekpad.com>
2024-10-21 22:27:11 -0400

    Implement "known-issue" workaround for v13
    There is a "known issue" with classic-level.
    The release notes prescribe manually installing the package.
    See:
    - https://foundryvtt.com/releases/13.332

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -198,6 +198,13 @@ if [ $install_required = true ]; then
     exit 1
   fi
 
+  # TODO: This is a workaround for a "known issue" with FoundryVTT 13.332
+  # Install classic-level module per release notes.
+  pushd resources/app > /dev/null
+  log "Installing classic-level module."
+  npm install classic-level --silent --no-audit --no-fund --no-progress
+  popd > /dev/null
+
   if [[ "${CONTAINER_CACHE:-}" ]]; then
     log "Preserving release archive file in cache."
     # Check if CONTAINER_CACHE_SIZE is set and if so, ensure it's greater than 0
